8cdd6c5c8f64aec53da767a4f1c192583709e2fd
Harsh0312 <68933179+Harsh0312@users.noreply.github.com>
2022-07-29 17:43:47 +0530

    New practise file added

---
+++ Practise.cs
@@ -0,0 +1,10 @@
+using System;
+class Practise{
+    static public void main(){
+        byte[] B={2,4,6,8,10};
+        Console.WriteLine("BArray is:{0}",BitConverter.ToString(B));
+        Console.WriteLine();
+        String str= Convert.ToBase64String(B);
+        Console.WriteLine("Base 64 string is {0}",str);
+    }
+}
